2b3f002d449889109c9f54724fee788e8dd3475e
Vitor Emanuel <veslima3@gmail.com>
2019-04-11 14:02:36 -0300

    Extract Matches
    So far, you have only been checking if a pattern exists or not within a string. You can also extract the actual matches you found with the .match() method.
    
    To use the .match() method, apply the method on a string and pass in the regex inside the parentheses. Here's an example:
    
    "Hello, World!".match(/Hello/);
    // Returns ["Hello"]
    let ourStr = "Regular expressions";
    let ourRegex = /expressions/;
    ourStr.match(ourRegex);
    // Returns ["expressions"]
    
    Apply the .match() method to extract the word coding.

---
+++ Extract Matches.js
@@ -0,0 +1,7 @@
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(/coding/); // Change this line
+
+/*or this version let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result = extractStr.match(codingRegex);*/
