1f65ec4efbde3a0a762a265143e892f09d530685
Vitor Emanuel <veslima3@gmail.com>
2019-04-14 00:23:04 -0300

    Match Single Characters Not Specified
    So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
    
    To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
    
    For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
    
    
    Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.

--- Match Single Characters Not Specified.js
+++ Match Single Characters Not Specified.js
@@ -1,3 +1,3 @@
 let quoteSample = "3 blind mice.";
-let myRegex = /[^aeiou0-99]/gi; // Change this line
+let myRegex = /[^aeiou^0-99]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
