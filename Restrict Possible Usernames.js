9473f34b06648ba3fbbb58935a556b816b672092
Vitor Emanuel <veslima3@gmail.com>
2019-04-16 11:04:29 -0300

    Restrict Possible Usernames
    Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
    
    You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
    
    1) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
    
    2) Username letters can be lowercase and uppercase.
    
    3) Usernames have to be at least two characters long. A two-letter username can only use alphabet letter characters.
    
    
    Change the regex userCheck to fit the constraints listed above.

--- Restrict Possible Usernames.js
+++ Restrict Possible Usernames.js
@@ -1,3 +1,9 @@
 let username = "JackOfAllTrades";
-let userCheck = /^[a-z]{2,}\d*$/i; // Change this line
+let userCheck = /[a-z]{2,}[0-9]*$/i; // Change this line
 let result = userCheck.test(username);
+
+/*let username = "JackOfAllTrades";
+let userCheck = /[a-z]{2,}\d*$/i; // Change this line
+let result = userCheck.test(username);*/
+
+/*There's no need to matche the position just before the first character of the string, so I took out the ^ (caret). Also, just to experiment, I took out the \d (digits) to see if it would work, and it did. \w breaks the code if used to replace [a-z] 
